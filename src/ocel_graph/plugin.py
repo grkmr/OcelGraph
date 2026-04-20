75ab7cb8ba539ddcbc377b7b768a711112826551
grkmr <goerkem@posteo.com>
2026-04-20 13:33:34 +0200

    :recycle: Move input definition into top level plugin path

--- src/ocel_graph/plugin.py
+++ src/ocel_graph/plugin.py
@@ -1,10 +1,53 @@
 from typing import Annotated
 
-from ocelescope import OCEL, OCELAnnotation, Plugin, plugin_method
+from ocelescope import OCEL, OCEL_FIELD, OCELAnnotation, Plugin, PluginInput, plugin_method
+from pydantic import BaseModel, Field
 
-from .inputs.ocelGraph import OCELGraphInput
 from .resources.ocelGraph import OCELGraph
 from .util import mine_ocel_graph
+
+# region Input definition
+
+
+class ObjectRoot(BaseModel):
+    class Config:
+        title = "Object"
+
+    object_id: str = OCEL_FIELD(
+        field_type="object_id",
+        title="Object Id",
+        ocel_id="ocel",
+        description="The ID of the Event which is the root of the OcelGraph",
+    )
+
+
+class EventRoot(BaseModel):
+    class Config:
+        title = "Event"
+
+    event_id: str = OCEL_FIELD(
+        field_type="event_id",
+        title="Event Id",
+        ocel_id="ocel",
+        description="The ID of the Event which is the root of the OcelGraph",
+    )
+
+
+class OCELGraphInput(PluginInput):
+    root: ObjectRoot | EventRoot
+    depth: int = Field(
+        title="OCEL Graph Depth", description="The maximum depth of the ocel graph", default=3, gt=0, le=10
+    )
+    max_neighbours: int = Field(
+        title="Maximum Neighbours", description="The maximum amount of neighbours a node can have", gt=0, default=5
+    )
+
+
+# endregion
+
+# region Resource Defintion
+
+# endregion
 
 
 class OcelGraphDiscovery(Plugin):
